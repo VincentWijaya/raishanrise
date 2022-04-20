62043c310842bd66d31c5f91d49b2a7565c6df07
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-04-20 20:19:55 +0700

    update font size for navbar

--- components/Navigation/Logo.tsx
+++ components/Navigation/Logo.tsx
@@ -4,7 +4,7 @@ import { Box, Text, useColorModeValue } from "@chakra-ui/react"
 export default function Logo(props) {
   return (
     <Box {...props}>
-      <Text fontSize="lg" fontWeight="bold" color={useColorModeValue('black', 'white')} px={4}>
+      <Text fontSize="2xl" fontWeight="bold" color={useColorModeValue('black', 'white')} px={4}>
         Raisha Syifa.
       </Text>
     </Box>
