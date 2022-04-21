16cb9be8866b55b4be500b56f807f1ae7516494d
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-04-21 13:36:09 +0700

    update footer and add some secre

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,25 +21,24 @@ const SocialButton = ({
   href: string;
 }) => {
   return (
-    <Link href={href} isExternal>
-      <chakra.button
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        w={8}
-        h={8}
-        cursor={'pointer'}
-        as={'a'}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        }}>
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    </Link>
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeProvider } from "@chakra-ui/react"
 import Head from 'next/head'
 
+const poorMe = () => {
+  console.log('%c Made with ❤️ by :', 'font-size: 20px')
+  console.log('%c kuli_coding', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)')
+  console.log(`%c Even though I'm not getting paid from this 😢`, 'font-size: 10px')
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
@@ -15,6 +21,7 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <ColorModeProvider options={{ initialColorMode: 'dark', useSystemColorMode: true }} />
       <Component {...pageProps} />
+      { poorMe() }
     </ChakraProvider>
   )
 }
