51eb91a16fa4c891f9e4546fc860584e84f07311
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-04-21 22:12:27 +0700

    add firestore and update stats component

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -4,13 +4,33 @@ import {
   Text,
   StackDivider
 } from '@chakra-ui/react'
+import { db } from '../service/firebase'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
 
 export default function Stats() {
-  const stats = {
-    show: '78',
-    setlist: '1',
-    unitSong: '1'
+  const [stats, setStats] = useState({
+    show: '0 Show',
+    setlist: '0 Setlist',
+    unitSong: '0 Unit Song'
+  })
+
+  const getStats = () => {
+    onSnapshot(collection(db, 'stats'), snapshot => {
+      snapshot.docs.map(doc => {
+        setStats(stats => ({
+          ...stats,
+          show: doc.data().show,
+          setlist: doc.data().setlist,
+          unitSong: doc.data().unitSong
+        }))
+      })
+    })
   }
+
+  useEffect(() => {
+    getStats()
+  }, [])
 
   return (
     <Flex alignItems='center' maxW={'10xl'} mt='7vh' bgColor='blue.400' height='35%'>
@@ -21,16 +41,16 @@ export default function Stats() {
         mx='auto'
       >
 
-      <Text mt='2vh' fontSize='3xl' opacity='0.9'>
-        {stats.show} Show
+      <Text fontSize='3xl' opacity='0.9'>
+        {stats.show}
       </Text>
 
       <Text fontSize='3xl' opacity='0.9'>
-        {stats.unitSong} Unit Song
+        {stats.unitSong}
       </Text>
 
       <Text fontSize='3xl' opacity='0.9'>
-        {stats.setlist} Setlist
+        {stats.setlist}
       </Text>
 
       </VStack>
