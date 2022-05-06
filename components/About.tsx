3ef60c8b501444b89e58bfccaa16406cd980b6a4
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-05-06 21:44:56 +0700

    add about page

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,8 @@ import {
   Text,
   Container,
   Stack,
-  Heading
+  Heading,
+  Avatar
 } from "@chakra-ui/react"
 import NavBar from './NavBar'
 import Footer from './Footer'
@@ -28,41 +29,27 @@ export default function Layout() {
             <Box h='calc(100vh)'>
                 <NavBar />
 
-                <Container maxW={'7xl'} pt='5vh' height={'100%'}>
+                <Container maxW={'7xl'} pt='10vh' height={'100%'}>
                   <Stack
                     align={'center'}
                     spacing={{ base: 8, md: 10 }}
                     py={{ base: 20, md: 28 }}
-                    direction={{ base: 'column', md: 'row' }}
+                    pb='30vh'
+                    direction={{ base: 'column', sm: 'row' }}
                     >
-                    <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-                      <Heading
-                        lineHeight={1.1}
-                        fontWeight={600}
-                        fontSize={{ base: '2xl', sm: '4xl', lg: '6xl' }}>
-                        <Text
-                          as={'span'}
-                          position={'inherit'}
-                          _after={{
-                            content: "''",
-                            width: 'full',
-                            height: '20%',
-                            position: 'absolute',
-                            bottom: 1,
-                            left: 0,
-                            zIndex: -1,
-                          }}>
-                          Raisha Syifa Wardhana
-                        </Text>
-                      </Heading>
-                      <Text color={'gray.500'}>
-                      Sesosok gadis bernama Raisha Syifa Wardhana yang memiliki nama panggilan Raisha, lahir pada 11 November 2007.
-                      Raisha Syifa sempat diumumkan menjadi member JKT48 generasi 10 pada tanggal 27 Agustus 2020, namun sayang karena efek pandemi, 
-                      pengumuman member generasi 10 ditunda hingga bulan Desember 2021. Setelah beberapa bulan menjalani trainee di JKT48, 
-                      akhirnya pada 26 Maret 2022, Raisha berkesempatan untuk melakukan shonichi di setlist Seishun Girls. Raisha Memiliki hobi bermain 
-                      bulutangkis dan basket. Walaupun masih berusia 15 tahun, namun Raisha merupakan member tertinggi di generasinya.
+                      <Avatar
+                        size={'3xl'}
+                        src='/images/raishanrise.jpeg'
+                        css={{
+                          border: '2px solid white',
+                        }}
+                      />
+
+                      <Text color={'gray.500'} align='center'>
+                      Raishanrise, merupakan Fanbase utama dari member generasi 10, Raisha Syifa Wardhana. Raishanrise sendiri diambil dari 2 kata yaitu Raisha n Rise, 
+                      yang memiliki arti Raisha berkembang, atau bisa diartikan juga terbitnya Raisha, Raishanrise memiliki cita-cita untuk mendukung dalam semua harapan 
+                      Raisha di JKT48 maupun diluar dari itu. 
                       </Text>
-                    </Stack>
                   </Stack>
                 </Container>
 
