504a291b0c5b4f70b7c6984f6dc62d79ffb4d8a4
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-05-16 06:41:08 +0700

    add register page

---
+++ components/Register.tsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Text,
+  Container,
+  Stack,
+  Avatar
+} from "@chakra-ui/react"
+import NavBar from './NavBar'
+import Footer from './Footer'
+import SEO from './SEO'
+
+export default function Layout() {
+    const desc = 'Raishanrise, merupakan Fanbase utama dari member generasi 10, Raisha Syifa Wardhana. Raishanrise sendiri diambil dari 2 kata yaitu Raisha n Rise, yang memiliki arti Raisha berkembang, atau bisa diartikan juga terbitnya Raisha, Raishanrise memiliki cita-cita untuk mendukung dalam semua harapan Raisha di JKT48 maupun diluar dari itu'
+    return (
+        <>
+            <Box h='calc(100vh)'>
+                <SEO description={ desc } image='https://raishanrise.my.id//images/raishanrise.jpeg' title='About - Raishanrise'/>
+                <NavBar />
+
+                <Container maxW={'7xl'} pt='10vh' height={'100%'}>
+                  <Stack
+                    align={'center'}
+                    spacing={{ base: 8, md: 10 }}
+                    py={{ base: 20, md: 28 }}
+                    pb='30vh'
+                    direction={{ base: 'column', sm: 'row' }}
+                    >
+                      <Avatar
+                        size={'3xl'}
+                        src='/images/raishanrise.jpeg'
+                        css={{
+                          border: '2px solid grey',
+                        }}
+                      />
+
+                      <Text color={'gray.500'} align='center'>{ desc }</Text>
+                  </Stack>
+                </Container>
+
+                <Footer />
+            </Box>
+        </>
+    )
+}
