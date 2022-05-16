9c60c646f2fe3379e82cb02f4eef377b58f1c996
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-05-16 09:01:24 +0700

    update register api to send data to telegram

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,9 +1,34 @@
+import axios from 'axios'
+import querystring from 'querystring'
+
 export default async (req, res) => {
   switch (req.method) {
-    case 'PSOT': {
+    case 'POST': {
       try {
-        console.log(req)
-        res.send(200).json({message: 'Success'})
+        res.send({message: 'Success'})
+        
+        const {
+          fullname,
+          nickname,
+          twitter,
+          line,
+          regional,
+          reason,
+          gender,
+          helping,
+          kas
+        } = req.body
+        const telegramData = {
+          chat_id: '-1001611289340',
+          text: `============================\nAda member baru nih!!!!!\n\nNama: ${fullname}\nPanggilan: ${nickname}\nJenis Kelamin: ${gender}\nTwitter: https://twitter.com/${twitter}\nLine:  http://line.me/ti/p/~${line}\nRegion: ${regional}\nAlasan join: ${reason}\nMau bantu project? ${helping}\nMau bayar uang kas? ${kas}`
+        }
+        axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, querystring.stringify(telegramData))
+          .then(resp => {
+            console.log('success send message')
+          })
+          .catch(err => {
+            console.log('failed send message', err)
+          })
       } catch (err) {
         console.log('Failed to send message to telegram', err)
         res.status(400).end()
