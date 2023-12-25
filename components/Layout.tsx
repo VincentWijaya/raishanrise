4ee73a371c8a8941b951a5ce8227448109f014ff
Vincent Wijaya <vincent.wijaya@mekari.com>
2023-12-25 16:05:44 +0700

    add vercel SpeedInsights

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,12 +4,14 @@ import Footer from './Footer'
 import Hero from '@components/Hero'
 import ScrollTop from './scrolltop'
 import SEO from './SEO'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout() {
     return (
         <>
             <Box h='calc(100vh)'>
                 <SEO />
+                <SpeedInsights/>
                 <NavBar />
 
                 <Hero />
