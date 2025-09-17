b6438742198e96d519177dd8e6d001631eaee65d
Vincent <vincent.wijaya@mekari.com>
2025-09-17 15:07:28 +0700

    update clg log

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,7 @@ import { EdgeStoreProvider } from '../lib/edgestore'
 
 const poorMe = () => {
   console.log('%c Made with ❤️ by :', 'font-size: 20px')
-  console.log('%c kuli_coding', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)')
-  console.log(`%c Even though I'm not getting paid from this 😢`, 'font-size: 10px')
+  console.log('%c VincentWijaya', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)')
 }
 
 function MyApp({ Component, pageProps }) {
