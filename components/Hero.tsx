35ad6bab859df77d1dda4b39043ebef8910bf6ca
Vincent <vincent.wijaya@mekari.com>
2025-09-18 10:50:42 +0700

    create countdown page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,7 @@ import axios from 'axios'
 import Twitter from './Twitter'
 import Instagram from './Instagram'
 import Tiktok from './Tiktok'
+import Countdown from './Countdown'
 
 export default function CallToActionWithVideo() {
   const [ isPlaying, setPlay ] = useState(false)
@@ -105,6 +106,8 @@ export default function CallToActionWithVideo() {
 
   return (
     <Container maxW={'7xl'} pt='5vh' height={'100%'}>
+      <Countdown />
+      
       <Stack
         align={'center'}
         spacing={{ base: 8, md: 10 }}
